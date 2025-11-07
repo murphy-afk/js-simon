287986681110920ff3342ec6b5b3edd390cbfcd2
murphy-afk <bardarenoemi@gmail.com>
2025-11-07 14:31:38 +0100

    feat: logic

--- script/script.js
+++ script/script.js
@@ -8,12 +8,28 @@
 // NOTA: non è importante l'ordine con cui l'utente inserisce i numeri,
 //  basta che ne indovini il più possibile.
 const numbers = [];
+const userNumbers = [3, 4, 5, 6, 7];
+
 
 while (numbers.length < 5) {
   const num = Math.floor(Math.random() * 10) + 1;
   if (numbers.includes(num) === false) numbers.push(num); 
 }
-console.log(numbers);
+console.log(`numeri random: ${numbers}`, `numeri user: ${userNumbers}`);
+
+
+for (let i = 0; i < userNumbers.length; i++) {
+  if (numbers.includes(userNumbers[i])) {
+    const guessed = userNumbers[i];
+    console.log(`numero indovinato: ${userNumbers[i]}`);
+    if (numbers[i] === guessed) {
+      console.log("posizione giusta");
+    }
+    else {
+      console.log("posizone sbagliata");
+    }
+  }
+}
 
 
 
